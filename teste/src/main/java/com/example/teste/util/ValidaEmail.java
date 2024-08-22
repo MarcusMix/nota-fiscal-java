1f2fefd177acf8f4d7609265bc37644c7d86974f
MARCUS VINÍCIUS SANDI <marcus.sandi@alunos.sc.senac.br>
2024-08-21 21:22:25 -0300

    feat: valida email com regex

--- teste/src/main/java/com/example/teste/util/ValidaEmail.java
+++ teste/src/main/java/com/example/teste/util/ValidaEmail.java
@@ -5,4 +5,13 @@ public class ValidaEmail {
     public static boolean validarCaracterArroba(String input) {
         return input != null && input.contains("@");
     }
+
+    public boolean isValid(String email) {
+        if (email == null) {
+            return false;
+        }
+
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return email.matches(emailRegex);
+    }
 }
